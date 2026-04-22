9f0a76dc3ab5d5ac0407feebe0129e48e5b6fa78
mesikaomri20-ops <mesikaomri20@gmail.com>
2026-04-22 13:49:28 +0300

    refactor: remove hardcoded keys and revert to env variables

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -6,8 +6,8 @@ export async function updateSession(request: NextRequest) {
     request,
   })
 
-  const url = 'https://swlfljsfujptehmawnai.supabase.co';
-  const key = 'sb_publishable_7Hn1_00pVP552e7qFcoJ3w_m4VTfEyI';
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
   const supabase = createServerClient(
     url,
